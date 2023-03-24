70038d386f987ed2015494024c311b912f897e7a
sathishkumar330 <svksathish41@gmail.com>
2023-03-24 16:03:44 +0530

    add

---
+++ main.js
@@ -0,0 +1,11 @@
+let date=new Date();
+	var b=(date.getHours());
+		if(b<=11){
+			console.log("morning");
+}
+		else if(b<=17){
+			console.log("Afternoon");
+}
+		else if(b<=22){
+			console.log("Evening");
+}
